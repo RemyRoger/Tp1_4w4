d5e4e299f2b77456ef291f84f52072ec7203bbb1
RemyRoger <e2364643@cmaisonneuve.qc.ca>
2025-02-27 11:37:01 -0500

    Carte et gabarit

---
+++ gabarit/carte.php
@@ -0,0 +1,22 @@
+<?php 
+/**Gabarit pour faire des cartes */
+?>
+
+
+<article class="carte caarte--grande">
+                     <figure class="carte__image">
+                        <img src="images/img1.jpg" alt="Image de voyage">
+                    </figure>
+                    <div class="carte__contenu">
+                        <?php 
+                            if (has_post_thumbnail()) {
+                            the_post_thumbnail('thumbnail'); } 
+                            ?>
+                    <h2 class="carte__titre"><?php the_title(); ?></h2>
+                    <p class="carte__description"><?php echo wp_trim_words(get_the_content(), 20, "..."); ?></p>
+                    <a href="<?php the_permalink() ?>"></a>
+                    <button class="carte__bouton carte__bouton--actif">Suite</button>
+                </div>
+                    <h2 class="populaire__titre"><?php the_title(); ?></h2>
+                    <div class="pouplaire__contenu"><?php echo wp_trim_words(get_the_content(), 20, "..."); ?></div>
+            </article>
