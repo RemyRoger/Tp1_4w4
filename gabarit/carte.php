ec6cb5a3d03cd29ee461fbd09a90977a1090c68d
RemyRoger <e2364643@cmaisonneuve.qc.ca>
2025-03-18 10:47:17 -0400

    CSS modifications

--- gabarit/carte.php
+++ gabarit/carte.php
@@ -13,7 +13,7 @@
         ?>
         <h2 class="carte__titre"><?php the_title(); ?></h2>
         <p class="carte__description"><?php echo wp_trim_words(get_the_content(), 20, "..."); ?></p>
-        <a href="<?php the_permalink() ?>" class="carte__bouton carte__bouton--actif">Suite</a>  
+        <a href="<?php the_permalink() ?>" class="carte__bouton carte__bouton--actif">Suite...</a>  
         <?php the_category() ?>
         <p>Température maximum :<?php echo the_field("temperature_maximum"); ?>°C</p>
         <p>Température minimum :<?php echo the_field("temperature_minimum"); ?>°C</p>
