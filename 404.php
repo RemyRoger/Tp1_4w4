7cea22c15f931947d0de14b6bbaa08b6ba24183c
RemyRoger <e2364643@cmaisonneuve.qc.ca>
2025-03-25 13:05:24 -0400

    Ajout d'un autre titre erreur 404 et css de la page 404

--- 404.php
+++ 404.php
@@ -3,11 +3,11 @@
 <?php $erreur_titre = get_theme_mod('erreur_titre', 'Default Title'); ?>
     <div class="erreur">
         <section class="erreur__section">
-            <h1><?php echo $erreur_titre ?></h1>
+            <h1 class="erreur__titre">ERREUR 404</h1>
+            <h2><?php echo $erreur_titre ?></h2>
             <img class="erreur__image" src="<?php echo $erreur_image ?>" alt="">
             <form method="post">
-            <p><a href="<?php echo home_url('/'); ?>">Retour à l'accueil</a></p>
-
+            <p class="retour__accueil"><a href="<?php echo home_url('/'); ?>">Retour à l'accueil</a></p>
        </section>
     </div>
 <?php get_footer() ?>
