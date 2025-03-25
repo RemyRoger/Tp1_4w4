34aa137727503497e95973910cbc71f5abae090a
RemyRoger <e2364643@cmaisonneuve.qc.ca>
2025-03-25 11:57:56 -0400

    Ajout d'un bouton pour retour a l'accueil

--- 404.php
+++ 404.php
@@ -1,14 +1,10 @@
 <?php get_header() ?>
 <?php $erreur_image = get_theme_mod('erreur_image', 'Default Title'); ?>
 <?php $erreur_titre = get_theme_mod('erreur_titre', 'Default Title'); ?>
-    <div>
-        <section class="erreur">
+    <div class="erreur">
+        <section class="erreur__section">
             <h1><?php echo $erreur_titre ?></h1>
             <img class="erreur__image" src="<?php echo $erreur_image ?>" alt="">
-            <?php wp_nav_menu(array(
-                'menu'  => 'Erreur  ',
-		        'container'  => 'div',
-		        'container_class'=> '',
-            )); ?>
+            <button>Retour à l'accueil</button>
     </div>
 <?php get_footer() ?>
