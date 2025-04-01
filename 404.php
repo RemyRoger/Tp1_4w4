a49a595b6c6f579f8808ff699ef00c0091d6584e
RemyRoger <e2364643@cmaisonneuve.qc.ca>
2025-04-01 10:12:30 -0400

    Debut css de la page 404

--- 404.php
+++ 404.php
@@ -2,11 +2,16 @@
 <?php $erreur_image = get_theme_mod('erreur_image', 'Default Title'); ?>
 <?php $erreur_titre = get_theme_mod('erreur_titre', 'Default Title'); ?>
 <?php $erreur_background = get_theme_mod('erreur_background', 'Default Title'); ?>
-
+<?php $couleur_pageErreur = get_theme_mod('$couleur_pageErreur', '#fff') ?>
+<style>
+    .erreur__section{
+        color:  <?php echo $couleur_pageErreur ?>;
+    }
+</style>
     <div class="erreur__div" style="background-image: url('<?php echo $erreur_background ?>'); background-repeat : no-repeat; width: 100%; max-width: 100vw; height: 1080px; max-height: 100%" >
         <section class="erreur__section">
-            <h1><?php echo $erreur_titre ?></h1>
-            <p>Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d’exception !</p>    
+            <h1 class="erreur__titre"><?php echo $erreur_titre ?> </h1>
+            <p class="erreur__para">Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d’exception !</p>    
             <p class="retour__accueil"><a href="<?php echo home_url('/'); ?>">Retour à l'accueil</a></p>
         </section>
     </div>
