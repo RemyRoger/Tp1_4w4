38b159321d13671dd6c7e85e5e1d316e6ff515c8
RemyRoger <e2364643@cmaisonneuve.qc.ca>
2025-04-01 11:21:53 -0400

    Ajout du menu

--- 404.php
+++ 404.php
@@ -2,10 +2,13 @@
 <?php $erreur_image = get_theme_mod('erreur_image', 'Default Title'); ?>
 <?php $erreur_titre = get_theme_mod('erreur_titre', 'Default Title'); ?>
 <?php $erreur_background = get_theme_mod('erreur_background', 'Default Title'); ?>
-<?php $couleur_pageErreur = get_theme_mod('$couleur_pageErreur', '#fff') ?>
+<?php $couleur_page404 = get_theme_mod('couleur_page404', '#fff') ?>
 <style>
-    .erreur__section{
-        color:  <?php echo $couleur_pageErreur ?>;
+    .erreur__titre{
+        color:  <?php echo $couleur_page404 ?>;
+    }
+    .erreur__para{
+        color:  <?php echo $couleur_page404 ?>;
     }
 </style>
     <div class="erreur__div" style="background-image: url('<?php echo $erreur_background ?>'); background-repeat : no-repeat; width: 100%; max-width: 100vw; height: 1080px; max-height: 100%" >
@@ -13,6 +16,14 @@
             <h1 class="erreur__titre"><?php echo $erreur_titre ?> </h1>
             <p class="erreur__para">Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d’exception !</p>    
             <p class="retour__accueil"><a href="<?php echo home_url('/'); ?>">Retour à l'accueil</a></p>
+            <?php wp_nav_menu(array(
+                    "menu" => "erreur",
+                    "container" => "",
+                    "container_class" => "erreur__menu",
+                )); ?>
+                
+            <h2 class="erreur__recherche"><?php get_search_form(); ?></h2>
+            
         </section>
     </div>
 <?php get_footer() ?>
--- functions/customizer.php
+++ functions/customizer.php
@@ -107,12 +107,12 @@ $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'erreur
   'section' => 'erreur_section',
 )));
 ///////////////////couleur du texte de la page 404////////////////////////
-$wp_customize->add_setting('erreur_texte', array(
+$wp_customize->add_setting('couleur_page404', array(
   'default' => '',
   'sanitize_callback' => 'esc_url_raw',
 ));
 
-$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'erreur_texte', array(
+$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'couleur_page404', array(
   'label' => __('Couleur du texte', 'theme_31w'),
   'section' => 'erreur_section',
 )));
