7b8d864707ca3cdd07e2996031eca18496246f74
RemyRoger <e2364643@cmaisonneuve.qc.ca>
2025-04-29 11:51:26 -0400

    modif du footer et animations du hero

--- footer.php
+++ footer.php
@@ -1,56 +1,41 @@
-
-<footer>
-    <?php
+<?php
     $footer_mission = get_theme_mod('footer_mission', 'Default Title');
-    $couleur = get_theme_mod('footer_adresse', 'Default Title');
-    $footer_adresse = get_theme_mod('footer_adresse', 'Default Title');
-    $footer_telephone = get_theme_mod('footer_telephone', 'Default Title');
+    $footer_couleur_arriere = get_theme_mod('footer_couleur_arriere', '#ec880d');
+    /* permet d'afficher une avant le footer */
+    vague($footer_couleur_arriere)
+?>
 
-    vague($footer_couleur_arriere);
-    
+<footer style="background-color: <?= $footer_couleur_arriere ?> ">
 
-    ?>
-    
+
+
     <div class="piedpage global">
         <section class="piedpage__s1">
-        <div class="piedpage__s1__externe">
+         
                 <?php wp_nav_menu(array(
-                    "menu" => "externe",
+                    "menu"=> "externe",
                     "container" => "nav",
                     "container_class" => "piedpage__s1__externe"
                 )); ?>
-            </div>
+    
+
             <div class="piedpage__s1__adresse">
- 
+                <div class="piedpage__s1__adresse__coord">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere porro veniam vitae, tempore corporis omnis nam 
+                </div>
                 <div class="piedpage__s1__adresse__recherche">
-                    <h3>Adresse et recherche</h3>
-                    <p><?php echo $footer_adresse; ?></p>
-                    
-                    <p>Tel: <?php echo $footer_telephone; ?></p>
                     <?php get_search_form();   ?>
                 </div>
             </div>
             <div class="piedpage__s1__description">
-                <h3>Mission du club</h3>
                 <?php echo $footer_mission; ?>
-                
             </div>
         </section>
-        <section class="piedpage__s2">
-                <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=paypal&color=000000" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="20" height="20">
-        </section>
-        <section class="piedpage__s3">
-        <?php wp_nav_menu(array(
-                    "menu" => "principale",
-                    "container" => "nav",
-                    "container_class" => ""
-                )); ?>
-        </section>
- 
- 
+        <section class="piedpage__s2"></section>
+      
+        
+
+
     </div>
 </footer>
 <?php wp_footer() ?>
--- gabarit/hero.php
+++ gabarit/hero.php
@@ -1,52 +1,50 @@
-<?php $hero_auteur = get_theme_mod('hero_auteur', 'Default Title'); ?>
-<?php for ($k=0; $k<3; $k++ ){
-    $hero_background[$k] = get_theme_mod('hero_background_' . $k, 'Default Title');
-} ?>
+<?php  
+$hero_auteur = get_theme_mod('hero_auteur', 'Default Title'); 
 
-<?php $couleur = substr(get_theme_mod('hero_icone', '#fff') ,1 )?>
-<?php $couleur_texte = get_theme_mod('hero_icone', '#fff') ?>
-<style>
-    .hero{
-        color:  <?php echo $couleur_texte ?>;
-    }
-</style>
-<!-- //////////////////////////Hero carroussel///////////////////////////////////-->
-    <section class="hero" >
-        <div class="hero__carrousel" style="background-image: url('<?php echo $hero_background[0] ?>')"></div>
-        <div class="hero__carrousel" style="background-image: url('<?php echo $hero_background[1] ?>')"></div>
-        <div class="hero__carrousel" style="background-image: url('<?php echo $hero_background[2] ?>')"></div>
-        <div class="hero__radio">
-            <input class="hero__radio__input" type="radio" name="carrousel" data-id_carrousel="0">
-            <input class="hero__radio__input" type="radio" name="carrousel" data-id_carrousel="1">
-            <input class="hero__radio__input" type="radio" name="carrousel" data-id_carrousel="2">
-
-        </div>
-<!-- //////////////////////////Hero contenu///////////////////////////////////-->
-        <div class="hero__contenu global">
+for ($k=0; $k<3; $k++){
+$hero_background[$k] = get_theme_mod('hero_background_' . $k, 'Default Title'); 
+}
+?>
+<section class="hero">
+    <!-- ///////////////////////////////////////////////// hero__carrousel -->
+    <div class="hero__carrousel"  style="background-image: url('<?php echo $hero_background[0] ?>');" ></div>    
+    <div class="hero__carrousel"  style="background-image: url('<?php echo $hero_background[1] ?>');" ></div> 
+    <div class="hero__carrousel"  style="background-image: url('<?php echo $hero_background[2] ?>');" ></div> 
+    <div class="hero__radio">
+        <input class="hero__radio__input" type="radio" name="carrousel" data-id_carrousel="0"  checked="checked">
+        <input class="hero__radio__input" type="radio" name="carrousel" data-id_carrousel="1">
+        <input class="hero__radio__input" type="radio" name="carrousel" data-id_carrousel="2">
+    </div>
+    <!-- ///////////////////////////////////////////////// hero__contenu -->
+    <div class="hero__contenu global">
+        <div class="hero__animation">
             <h1 class="hero__titre">
-                <?php echo bloginfo('name') ?>
+                <?php  bloginfo('name'); ?>
             </h1>
             <p class="hero__description">
-                <?php echo bloginfo('description') ?>
+            <?php  bloginfo('description'); ?>
             </p>
-            <p class="hero__courriel">
-                info@cmaisonneuve.qc.ca
+        </div>
+        <div class="hero__animation">
+            <h1 class="hero__titre">
+                Lorem ipsum dolor
+            </h1>
+            <p class="hero__description">
+            consectetur adipisicing elit. Dicta velit asperiores 
             </p>
-            <p class="hero__addresse">
-                3800, rue Sherbrooke, Montreal
-            </p>
-            <p class="hero__numero">
-                514-254-7131
-            </p>
-            <button class="hero__bouton">
-                s'inscrire
-            </button>
-            <div class="hero__icone-app">
-                <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=<?php echo $couleur ?>" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=<?php echo $couleur ?>" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=paypal&color=<?php echo $couleur ?>" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=<?php echo $couleur ?>" width="20" height="20">
-            </div>
-            <p> Auteur: <?php echo $hero_auteur ?></p>
+        </div>
+        <a href="" class="hero__courriel">
+            info@cmaisonneuve.qc.ca
+        </a>
+        <button class="hero__bouton">
+            Inscription
+        </button>
+        <div class="hero__icone-app">
+            <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
+            <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
+            <img src="https://s2.svgbox.net/social.svg?ic=paypal&color=000000" width="20" height="20">
+            <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="20" height="20">
+        </div>
+        <p>Auteur:<?php echo $hero_auteur;  ?></p>
         </div>
     </section>
